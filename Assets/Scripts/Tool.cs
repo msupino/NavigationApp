696588685b5c37756be376ab4337bae0cc3af923
Marco <marco@supino.org>
2019-10-07 14:01:38 +0300

    started working on the toolbar

---
+++ Assets/Scripts/Tool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Tools;
+using MainLogic;
+
+public class Tool : MonoBehaviour
+{
+    public MainLoop main;
+    public Tooltype tool;
+    private Image image;
+    public bool isActive = false;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+        image.color = new Color32(255, 255, 255, 100);
+        gameObject.GetComponent<Button>().onClick.AddListener(Clicked);
+        main.toolbar.buttons.Add(gameObject);
+        Refresh();
+    }
+
+    void Clicked()
+    {
+        isActive = !isActive;
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        if (isActive)
+        {
+            image.color = new Color32(255, 255, 255, 255);
+            main.toolbar.SetTool(tool);
+
+            foreach (GameObject btn in main.toolbar.buttons)
+            {
+                if (btn != gameObject)
+                {
+                    btn.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+                    btn.GetComponent<Tool>().isActive = false;
+
+                }
+            }
+        }
+    }
+    
+
+}
---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+
+    public enum Tooltype
+    {
+        None,
+        Draw,
+        Erase
+    }
+
+    public class Toolbar
+    {
+        public Tooltype currentTool;
+        public List<GameObject> buttons = new List<GameObject>();
+
+        public Toolbar()
+        {
+
+        }
+
+        public void SetTool(Tooltype t)
+        {
+            currentTool = t;
+            Debug.Log("The current tool is:" + t);
+        }
+    }
+
+}
