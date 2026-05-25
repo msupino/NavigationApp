70831aefe82b5d621782adbe5dec29b9ce493e52
Marco Supino <marco@supino.org>
2026-05-25 21:23:59 +0300

    use separate export keys with Print prefix matching View labels

--- docs/he/strings.js
+++ docs/he/strings.js
@@ -171,7 +171,8 @@ window.S = {
   tbViewSource: 'GitHub',
   tbWiki: 'Wiki',
   exportModalTitle: 'ייצוא PNG',
-
+  exportShowNavWP: 'הדפס ציוני ניווט',
+  exportShowAirfields: 'הדפס שדות תעופה',
   exportLayer: 'שכבת מפה',
   exportBtn: 'ייצא',
 };
